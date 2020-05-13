abc5f91c58dfa27fba2a8bbee1e781d15c9f36bd
Pearl Jean <perlegirl@protonmail.com>
2020-05-12 20:21:46 -0400

    Initial commit - not really initial but oh well

---
+++ app/src/main/java/com/zwartezwaan/quanumobjcatalog/Shape.kt
@@ -0,0 +1,13 @@
+package com.zwartezwaan.quanumobjcatalog
+
+class Shape{
+    var name:String?=null
+    var desc:String?=null
+    var img:Int?=null
+
+    constructor(namey:String, descy:String, imgy:Int){
+        this.name = namey
+        this.desc = descy
+        this.img = imgy
+    }
+}
